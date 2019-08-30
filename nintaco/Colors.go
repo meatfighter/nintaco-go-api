6ca073fefbe83cf3f77a090080ad3bb65cf68a79
Michael Birken <o__1@hotmail.com>
2019-08-30 11:11:16 -0400

    constants

---
+++ nintaco/Colors.go
@@ -0,0 +1,72 @@
+package nintaco
+
+// Colors
+const (
+	ColorGray             = 0x00
+	ColorDarkBlue         = 0x01
+	ColorDarkIndigo       = 0x02
+	ColorDarkViolet       = 0x03
+	ColorDarkMagenta      = 0x04
+	ColorDarkRed          = 0x05
+	ColorDarkOrange       = 0x06
+	ColorDarkBrown        = 0x07
+	ColorDarkOlive        = 0x08
+	ColorDarkChartreuse   = 0x09
+	ColorDarkGreen        = 0x0A
+	ColorDarkMint         = 0x0B
+	ColorDarkCyan         = 0x0C
+	ColorBlackerThanBlack = 0x0D
+	ColorBlack3           = 0x0E
+	ColorBlack            = 0x0F
+
+	ColorLightGray  = 0x10
+	ColorBlue       = 0x11
+	ColorIndigo     = 0x12
+	ColorViolet     = 0x13
+	ColorMagenta    = 0x14
+	ColorRed        = 0x15
+	ColorOrange     = 0x16
+	ColorBrown      = 0x17
+	ColorOlive      = 0x18
+	ColorChartreuse = 0x19
+	ColorGreen      = 0x1A
+	ColorMint       = 0x1B
+	ColorCyan       = 0x1C
+	ColorBlack2     = 0x1D
+	ColorBlack4     = 0x1E
+	ColorBlack5     = 0x1F
+
+	ColorWhite           = 0x20
+	ColorLightBlue       = 0x21
+	ColorLightIndigo     = 0x22
+	ColorLightViolet     = 0x23
+	ColorLightMagenta    = 0x24
+	ColorLightRed        = 0x25
+	ColorLightOrange     = 0x26
+	ColorLightBrown      = 0x27
+	ColorLightOlive      = 0x28
+	ColorLightChartreuse = 0x29
+	ColorLightGreen      = 0x2A
+	ColorLightMint       = 0x2B
+	ColorLightCyan       = 0x2C
+	ColorDarkGray        = 0x2D
+	ColorBlack6          = 0x2E
+	ColorBlack7          = 0x2F
+
+	ColorWhite2         = 0x30
+	ColorPaleBlue       = 0x31
+	ColorPaleIndigo     = 0x32
+	ColorPaleViolet     = 0x33
+	ColorPaleMagenta    = 0x34
+	ColorPaleRed        = 0x35
+	ColorPaleOrange     = 0x36
+	ColorCream          = 0x37
+	ColorYellow         = 0x38
+	ColorPaleChartreuse = 0x39
+	ColorPaleGreen      = 0x3A
+	ColorPaleMint       = 0x3B
+	ColorPaleCyan       = 0x3C
+	ColorPaleGray       = 0x3D
+	ColorBlack8         = 0x3E
+	ColorBlack9         = 0x3F
+)
---
+++ nintaco/GamepadButtons.go
@@ -0,0 +1,13 @@
+package nintaco
+
+// GamepadButtons
+const (
+	GamepadButtonA      = 0
+	GamepadButtonB      = 1
+	GamepadButtonSelect = 2
+	GamepadButtonStart  = 3
+	GamepadButtonUp     = 4
+	GamepadButtonDown   = 5
+	GamepadButtonLeft   = 6
+	GamepadButtonRight  = 7
+)
