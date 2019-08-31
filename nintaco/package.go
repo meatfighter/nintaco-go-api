ca2012792b7162bc92d58fa1ad6ba40c226d7ccf
Michael Birken <o__1@hotmail.com>
2019-08-31 16:34:21 -0400

    writing readme

---
+++ nintaco/package.go
@@ -0,0 +1,5 @@
+// Package nintaco provides programmatic control of the Nintaco NES/Famicom
+// emulator at a very granular level. It supports direct access to CPU/PPU/APU
+// Memory and registers, to controller input and to save states. And it offers
+// a multitude of listeners that enable programs to tap into emulation events.
+package nintaco
