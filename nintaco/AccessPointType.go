d8590f7d3f65445d5914f3c48d21af0107dedf6b
Michael Birken <o__1@hotmail.com>
2019-08-31 11:50:04 -0400

    AccessPointType

---
+++ nintaco/AccessPointType.go
@@ -0,0 +1,11 @@
+package nintaco
+
+// AccessPointType ...
+const (
+	AccessPointTypePreRead     = 0
+	AccessPointTypePostRead    = 1
+	AccessPointTypePreWrite    = 2
+	AccessPointTypePostWrite   = 3
+	AccessPointTypePreExecute  = 4
+	AccessPointTypePostExecute = 5
+)
